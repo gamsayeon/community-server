345b7d7ebc916c3287bc52df6171c1882d06abd0
gamsayeon <aud4551@naver.com>
2023-01-12 22:15:45 +0900

    ENH: UserService 단위테스트 부족한 부분 보충 및 리팩토링

--- src/test/java/com/communityserver/service/UserServiceTest.java
+++ src/test/java/com/communityserver/service/UserServiceTest.java
@@ -12,7 +12,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
@@ -27,8 +27,9 @@ public class UserServiceTest {
     private final int noPermissionAdmin = 1;
     private final int notSecession = 0;
     private final int testUserNumber = 1;
-    private final int testFailUserNumber =2;
-    public UserDTO generateTestUser(){
+    private final int testFailUserNumber = 2;
+
+    public UserDTO generateTestUser() {
         MockitoAnnotations.initMocks(this); // mock all the field having @Mock annotation
         UserDTO userDTO = new UserDTO();
         userDTO.setUserNumber(testUserNumber);
@@ -43,14 +44,15 @@ public class UserServiceTest {
 
     @Test
     @DisplayName("유저 회원가입 성공 테스트")
-    public void signUpSuccessTest(){
+    public void signUpSuccessTest() {
         final UserDTO userDTO = generateTestUser();
-        userService.register(userDTO);
+        UserDTO response = userService.register(userDTO);
+        assertEquals(userDTO, response);
     }
 
     @Test
-    @DisplayName("유저 회원가입 실패 테스트 (1. 아이디 중복)")
-    public void signUpFailTest(){
+    @DisplayName("유저 회원가입 실패 테스트 (1. 아이디 중복, 2. 비밀번호 오타, 3. 특수문자 사용, 4. 아이디 문자길이 제한)")
+    public void signUpFailTest() {
         // 1. 아이디 중복 (예외가 커스텀 예외중 중복 아이디 예외가 발생하는지)
         final UserDTO userDTO = generateTestUser();
         final UserDTO emptyUserDTO = UserDTO.builder().build();
@@ -60,42 +62,36 @@ public class UserServiceTest {
 
     @Test
     @DisplayName("유저 로그인 성공 테스트")
-    public void loginUserSuccessTest(){
+    public void loginUserSuccessTest() {
         UserDTO userDTO = generateTestUser();
         given(userMapper.passwordCheck("testUserId", sha256Encrypt.encrypt("testUserPassword"))).willReturn(userDTO);
-        assertThat(userService.LoginCheckPassword("testUserId","testUserPassword").equals(userDTO));
+        assertThat(userService.LoginCheckPassword("testUserId", "testUserPassword").equals(userDTO));
     }
 
     @Test
     @DisplayName("유저 로그인 실패 테스트")
-    public void loginUserFailTest(){
+    public void loginUserFailTest() {
         UserDTO userDTO = generateTestUser();
         UserDTO notExistUser = UserDTO.builder().build();
         //given(userMapper.passwordCheck("testUserId", sha256Encrypt.encrypt("testFailUserPassword"))).willReturn(null);
-        assertEquals(userService.LoginCheckPassword("testUserId","testFailUserPassword").getUserNumber()
+        assertEquals(userService.LoginCheckPassword("testUserId", userDTO.getPassword()+"failPassword").getUserNumber()
                 , notExistUser.getUserNumber());
     }
 
     @Test
     @DisplayName("유저 정보 확인 성공 테스트")
-    public void selectUserSuccessTest(){
+    public void selectUserSuccessTest() {
         UserDTO userDTO = generateTestUser();
         given(userMapper.selectUser(testUserNumber)).willReturn(userDTO);
         assertEquals(userMapper.selectUser(testUserNumber), userDTO);
     }
 
     @Test
-    @DisplayName("유저 정보 확인 실패 테스트")
-    public void selectUserFailTest(){
-        UserDTO userDTO = generateTestUser();
-        given(userMapper.selectUser(testUserNumber)).willReturn(userDTO);
-//        assertThat(userService.selectUser(testFailUserNumber).equals(userDTO));
-    }
-    @Test
     @DisplayName("회원 탈퇴 성공 테스트")
-    public void deleteUserSuccessTest(){
+    public void deleteUserSuccessTest() {
         UserDTO userDTO = generateTestUser();
         userService.deleteUser(userDTO.getUserNumber());
+        assertEquals(userService.selectUser(userDTO.getUserNumber()), null);
     }
 
 }
